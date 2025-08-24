4d2ec5be3e05ad9e54b7c59934d47444023873e8
Harsha Deepan <87807647+deepanharsha@users.noreply.github.com>
2025-08-24 17:17:30 +0530

    Add Instagram to ContactSection

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -42,7 +42,6 @@ export default function ContactSection() {
 								className="inline-block px-6 py-3 bg-green-500/10 text-green-400 rounded-lg border border-green-500/20 hover:bg-green-500/20 transition-colors"
 							>
 								theprojektmakers@gmail.com (Project Related)  <br />
-								harshadeepan11@gmail.com (Personal)
 							</motion.a>
 							{/*
 							<p className="text-green-500 mt-8 mb-2">$ cat resume.pdf</p>
@@ -76,23 +75,41 @@ export default function ContactSection() {
 										<p className="text-sm text-gray-400">@deepanharsha</p>
 									</div>
 								</motion.a>
-								{/*
-								<motion.a
-									href="#"
-									whileHover={{ scale: 1.05 }}
-									className="flex items-center gap-3 px-4 py-3 bg-gray-800/50 rounded-lg hover:bg-gray-700/50 transition-colors border border-gray-700/50 group"
+								
+								<motion.a 
+								  href="https://www.instagram.com/notharsha7"
+								  target="_blank"
+								  rel="noopener noreferrer"
+								  className="flex items-center gap-3 p-2 bg-gray-700/50 rounded-lg group-hover:bg-gray-600/50 transition-colors"
 								>
-									<div className="p-2 bg-gray-700/50 rounded-lg group-hover:bg-gray-600/50 transition-colors">
-										<svg className="w-5 h-5 text-green-400" fill="currentColor" viewBox="0 0 24 24">
-											<path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
-										</svg>
-									</div>
-									<div>
-										<p className="font-semibold text-gray-200">LinkedIn</p>
-										<p className="text-sm text-gray-400">Alex Chen</p>
-									</div>
+								  <div>
+								    <svg 
+								      className="w-5 h-5 text-pink-400" 
+								      fill="currentColor" 
+								      viewBox="0 0 169.063 169.063" 
+								      xmlns="http://www.w3.org/2000/svg"
+								    >
+								      <g>
+								        <path d="M122.406,0H46.654C20.929,0,0,20.93,0,46.655v75.752c0,25.726,20.929,46.655,46.654,46.655h75.752
+								                          c25.727,0,46.656-20.93,46.656-46.655V46.655C169.063,20.93,148.133,0,122.406,0z M154.063,122.407
+								                          c0,17.455-14.201,31.655-31.656,31.655H46.654C29.2,154.063,15,139.862,15,122.407V46.655C15,29.201,29.2,15,46.654,15h75.752
+								                          c17.455,0,31.656,14.201,31.656,31.655V122.407z" />
+								        <path d="M84.531,40.97c-24.021,0-43.563,19.542-43.563,43.563c0,24.02,19.542,43.561,43.563,43.561s43.563-19.541,43.563-43.561
+								                          C128.094,60.512,108.552,40.97,84.531,40.97z M84.531,113.093c-15.749,0-28.563-12.812-28.563-28.561
+								                          c0-15.75,12.813-28.563,28.563-28.563s28.563,12.813,28.563,28.563C113.094,100.281,100.28,113.093,84.531,113.093z" />
+								        <path d="M129.921,28.251c-2.89,0-5.729,1.17-7.77,3.22c-2.051,2.04-3.23,4.88-3.23,7.78c0,2.891,1.18,5.73,3.23,7.78
+								                          c2.04,2.04,4.88,3.22,7.77,3.22c2.9,0,5.73-1.18,7.78-3.22c2.05-2.05,3.22-4.89,3.22-7.78c0-2.9-1.17-5.74-3.22-7.78
+								                          C135.661,29.421,132.821,28.251,129.921,28.251z" />
+								      </g>
+								    </svg>
+								  </div>
+								  <div>
+								    <p className="font-semibold text-gray-200">Instagram</p>
+								    <p className="text-sm text-gray-400">@notharsha7</p>
+								  </div>
 								</motion.a>
-								*/}
+
+								
 								<motion.a
 									href="https://x.com/deepan_harsha"
 									whileHover={{ scale: 1.05 }}
